36a2ab51ff6edcb4667ec660147033055bb56b23
GulliWeb <andreagulli11@gmail.com>
2025-01-10 03:27:46 +0100

    Add Math.max method to return the larger value of two arguments

--- CsharpProjects/TestProjects/Program.cs
+++ CsharpProjects/TestProjects/Program.cs
@@ -1,4 +1,9 @@
-﻿Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
 
+largerValue = Math.Max(firstValue, secondValue);
+
+
+
+Console.WriteLine(largerValue);
